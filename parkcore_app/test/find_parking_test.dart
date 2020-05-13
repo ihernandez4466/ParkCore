38a43a1174e750e13655e3b453f3438e0c0bb631
Isabel Hernandez <isabelhernandez@isabels-mbp.attlocal.net>
2020-05-12 19:14:15 -0700

    Added tests

--- parkcore_app/test/find_parking_test.dart
+++ parkcore_app/test/find_parking_test.dart
@@ -13,4 +13,39 @@ void main() {
     final titleFinder = find.text('Find Parking');
     expect(titleFinder, findsOneWidget);
   });
+  testWidgets('Calculate Distance RaisedButton Field', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+      home: FindParking(title: 'Find Parking', city: 'Chico', latlong: '{39.7285,-121.8375}'),
+    ));
+
+    expect(find.widgetWithText(RaisedButton, 'Show distance to CSU, Chico'), findsOneWidget);
+  }); 
+
+  testWidgets('Haversize for Distance', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+      home: FindParking(title: 'Find Parking', city: 'Chico', latlong: '{39.7285,-121.8375}'),
+    ));
+
+    expect(FindParking().createState().haversize('{39.727580,-121.840430}'), '13');
+  });
+
+  testWidgets('Adjust Distance-Less than a Mile', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+      home: FindParking(title: 'Find Parking', city: 'Chico', latlong: '{39.7285,-121.8375}'),
+    ));
+
+    expect(FindParking().createState().adjustDistance(.5), .5);
+  });
+  
+  testWidgets('Adjust Distance-More than a Mile', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(MaterialApp(
+      home: FindParking(title: 'Find Parking', city: 'Chico', latlong: '{39.7285,-121.8375}'),
+    ));
+
+    expect(FindParking().createState().adjustDistance(3.6), 4.35);
+  });
 }
