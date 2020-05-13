d49764fdd89938de22177f32497ea053244a808d
Isabel Hernandez <isabelhernandez@isabels-mbp.attlocal.net>
2020-05-13 00:09:06 -0700

    Attempts

--- parkcore_app/test/parking_details_test.dart
+++ parkcore_app/test/parking_details_test.dart
@@ -1,16 +1,28 @@
-//import 'package:flutter/material.dart';
-//import 'package:flutter_test/flutter_test.dart';
-//import 'package:parkcore_app/parking/parking_details.dart';
-//import 'package:parkcore_app/parking/find_parking.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:parkcore_app/parking/parking_details.dart';
+import 'package:parkcore_app/parking/find_parking.dart';
+import 'package:parkcore_app/parking/parking_details.dart';
+import 'package:parkcore_app/parking/find_parking.dart';
 
 void main() {
+  
+  /*testWidgets('Find Availability', (WidgetTester tester) async {
+      // Build an app with a Text widget that displays the letter 'H'.
+      Spot spot;
+      await tester.pumpWidget(DetailScreen(spot: spot),
+      );
 
-//  testWidgets('Find Parking Details Page', (WidgetTester tester) async {
-//    // Build our app and trigger a frame.
-//    await tester.pumpWidget(MaterialApp(
-//      home: DetailScreen(spot: spot),
-//    ));
-//
-//    expect(find.bySemanticsLabel('My Title'), findsOneWidget);
-//  });
+      // Find a widget that displays the letter 'H'.
+      expect(find.byType(RaisedButton), findsOneWidget);
+  });
+  testWidgets('Find Size', (WidgetTester tester) async {
+      // Build an app with a Text widget that displays the letter 'H'.
+      Spot spot;
+      await tester.pumpWidget(DetailScreen(spot: spot),
+      );
+
+      // Find a widget that displays the letter 'H'.
+      expect(find.text('Size:'), findsOneWidget);
+  });*/
 }
